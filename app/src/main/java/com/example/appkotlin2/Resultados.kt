3e28e7ac9b2b119f0d57686a64e766b80a8d9a3d
Karen Agudelo <kdagudelo70@misena.edu.co>
2022-03-28 09:06:43 -1200

    Se empieza agregar el frameloyput en la actividad registro

--- app/src/main/java/com/example/appkotlin2/Resultados.kt
+++ app/src/main/java/com/example/appkotlin2/Resultados.kt
@@ -1,23 +1,11 @@
 package com.example.appkotlin2
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
-
+class Resultados : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        setContentView(R.layout.activity_resultados)
     }
-
-
-
-    
 }
-
-
-
