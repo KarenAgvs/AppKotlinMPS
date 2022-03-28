eeba3e8eb27c2c12f9dabeb785f7af35906e362a
Karen Agudelo <kdagudelo70@misena.edu.co>
2022-03-28 11:41:13 -1200

    Parametrizando textos de la aplicación

--- app/src/main/java/com/example/appkotlin2/MainActivity.kt
+++ app/src/main/java/com/example/appkotlin2/MainActivity.kt
@@ -1,16 +1,14 @@
 package com.example.appkotlin2
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
 
     }
 
