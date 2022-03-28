f6a5a56d352fd5ed06c29528e4cd2f7ac355ed9d
Karen Agudelo <kdagudelo70@misena.edu.co>
2022-03-27 21:44:58 -1200

    Creación de clases, splash screen, se agrega logo

---
+++ app/src/test/java/clases/Materia.kt
@@ -0,0 +1,13 @@
+package clases
+
+class Materia (nombre:String, nota:Double) {
+
+    var nombreMateria: String = nombre
+    var promedioMateria: Double = nota
+
+    override fun toString(): String {
+        return "Materia(nombreMateria='$nombreMateria', promedio materia=$promedioMateria)"
+
+
+    }
+}
