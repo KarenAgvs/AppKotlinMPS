734ef187b0b4ac22528c6730d36e4b24f58316c9
Karen Agudelo <kdagudelo70@misena.edu.co>
2022-03-28 04:14:20 -1200

    Personalización de mainActivity

--- app/src/main/java/com/example/appkotlin2/MainActivity.kt
+++ app/src/main/java/com/example/appkotlin2/MainActivity.kt
@@ -1,13 +1,23 @@
 package com.example.appkotlin2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
     }
 
+    
 }
+
+
 
