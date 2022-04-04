90b5f4f41f8f79ee773b59b3e32cbd3cd855fc2a
Karen Agudelo <kdagudelo70@misena.edu.co>
2022-04-04 00:01:14 -1200

    Agrego botones faltantes a los activitys

--- app/src/main/java/com/example/appkotlin2/Instrucciones.kt
+++ app/src/main/java/com/example/appkotlin2/Instrucciones.kt
@@ -1,11 +1,19 @@
 package com.example.appkotlin2
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class Instrucciones : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_instrucciones)
+        val buttnatras: Button =findViewById(R.id.buttonatras3)
+        buttnatras.setOnClickListener{atras()}
     }
-}
+    private fun atras(){
+        startActivity(Intent(this,MainActivity::class.java))
+    }
+    }
+
